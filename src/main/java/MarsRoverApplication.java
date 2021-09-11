6f9a5966973fd6a76f8228a8fe7867344d22d7d1
alper kopuz <alper.kopuz@ericsson.com>
2021-09-11 19:23:58 +0200

    TODOS were removed

--- src/main/java/MarsRoverApplication.java
+++ src/main/java/MarsRoverApplication.java
@@ -8,9 +8,6 @@ public class MarsRoverApplication {
 
     private static final Logger logger = LogManager.getLogger(MarsRoverApplication.class);
 
-    // TODO kullanicidan verileri girmesini iste (optional)
-    //REadme dosyasini guncelle mesela java ile nasil calistirilir anlatmaya calis
-
     private static MarsRoverService marsRoverService = new MarsRoverService();
 
     /**
@@ -22,8 +19,8 @@ public class MarsRoverApplication {
         MarsRoverApplication rover = new MarsRoverApplication();
         Axis axis = new Axis(4, 2, DirectionConstants.East);
         logger.info("Mars Rover Application will work from initial x coordinate" + 4 + " for y coordinate "
-        + 2 + " for Direction East" );
-        logger.info(" Commands executed for the mars rover is " + "FLFFFRFLB" );
+                + 2 + " for Direction East");
+        logger.info(" Commands executed for the mars rover is " + "FLFFFRFLB");
         marsRoverService.processCommands("FLFFFRFLB", axis);
         marsRoverService.showDirection(axis);
     }
